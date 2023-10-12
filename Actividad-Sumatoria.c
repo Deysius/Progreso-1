679e7b005da9b19e2ddba786d0dc49c1fed06323
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-12 16:17:48 -0500

    Pido al usuario los datos necesarios mediante un scanf, y mediante un if y un bucle while, verifico que el numero ingresado sea positivo y si no es el caso le pedi que ingrese uno nuevamente

--- Actividad-Sumatoria.c
+++ Actividad-Sumatoria.c
@@ -2,8 +2,14 @@
 int main(void){
 int numero;
 int suma;
-
-
-
+printf ("Ingrese un numero entero positivo\n");
+scanf ("%d",&numero);
+if (numero<=0){
+while (numero<=0)
+{
+    printf ("El numero ingresado no es un entero positivo, intente otra vez\n");
+    scanf ("%d",&numero);
+}
+}
 return 0;
 }
