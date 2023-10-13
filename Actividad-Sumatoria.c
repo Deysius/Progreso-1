16d4ca03d81dd570551841e3626c0b01ffa8cde5
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-12 20:04:51 -0500

    se agrego un menu para facilitar el ingreso de mas numeros

--- Actividad-Sumatoria.c
+++ Actividad-Sumatoria.c
@@ -2,16 +2,31 @@
 int main(void){
 int numero;
 int suma;
-printf ("Ingrese un numero entero positivo\n");
-scanf ("%d",&numero);
-if (numero<=0){
-while (numero<=0)
+int menu;
+while (menu!=2)
 {
+    printf ("(1)Suma de gauss\n");
+    printf ("(2)Salir\n");
+    scanf ("%d",&menu);
+    switch (menu)
+    {
+    case 1: 
+    printf ("Ingrese un numero entero positivo\n");
+    scanf ("%d",&numero);
+    if (numero<=0){
+    while (numero<=0){
     printf ("El numero ingresado no es un entero positivo, intente otra vez\n");
     scanf ("%d",&numero);
+    }
+    }
+    suma=(numero*(numero+1))/2;
+    printf ("La suma de los primeros %d numeros es: %d\n",numero,suma);
+    return 0;
+    break;
+    case 2:
+    break;
+    default: printf ("Opcion no valida\n");
+    break;
+    }
 }
-}
-suma=(numero*(numero+1))/2;
-printf ("La suma de los primeros %d numeros es: %d\n",numero,suma);
-return 0;
 }   
