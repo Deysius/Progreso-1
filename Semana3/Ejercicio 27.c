e12ab7c998a76acc204536044b895334abeb6c3c
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-17 07:13:21 -0500

    Agrego dos varibales, y pido al usuario que las de valor mediante un scanf

---
+++ Semana3/Ejercicio 27.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+int num1;
+int num2;
+printf ("Ingrese el primer numero a comparar\n");
+scanf ("%d",&num1);
+printf ("Ingrese el segundo numero a comparar\n");
+scanf ("%d",&num2);
+
+return 0;
+}
