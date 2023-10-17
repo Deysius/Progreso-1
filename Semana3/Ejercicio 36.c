d3bdf20cce9ec2e0ead9848d0e53bc534822b614
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-17 11:21:46 -0500

    Agrego un scanf para obtener el numero de datos que me debe mostar la tabla del 5

--- Semana3/Ejercicio 36.c
+++ Semana3/Ejercicio 36.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(void){
-int tabla5;
+int tabla5=0;
+int numtabla5;
 printf ("Indique cuantos numeros de la tabla del 5 decia ver\n");
-
+scanf ("%d",&numtabla5);
+printf ("La tabla del 5 es\n");
 return 0;
 }
