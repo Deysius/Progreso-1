0d7f275203fd1134ad393e88c9ff7346bbcc202f
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-17 07:42:24 -0500

    Añado 4 nuevas variables, dos de ellas funionan como contadores en un bucle while, mientras que las demas representan al numero ingresado respectivamente, posteriormente ellas son divididos para 10 sin decimales,hasta que llegen a 0

--- Semana3/Ejercicio 27.c
+++ Semana3/Ejercicio 27.c
@@ -2,10 +2,17 @@
 int main(void){
 int num1;
 int num2;
+int digitosnum1=0;
+int digitosnum2=0;
 printf ("Ingrese el primer numero a comparar\n");
 scanf ("%d",&num1);
 printf ("Ingrese el segundo numero a comparar\n");
 scanf ("%d",&num2);
-
-return 0;
+int resultnum1=num1;
+int resultnum2=num2;
+while (resultnum1!=0)
+{
+    digitosnum1++;
+    resultnum1/=10;
+}
 }
