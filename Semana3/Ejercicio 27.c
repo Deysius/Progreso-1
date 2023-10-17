a522d1d86656844435a0e2e002cbf86bedbbb088
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-17 07:44:35 -0500

    Añado otro while para realizar el mismo procedimiento con el segundo numero, al finalizar estos bucles, comparo mediante un if, cual de los contadores tiene mayor valor

--- Semana3/Ejercicio 27.c
+++ Semana3/Ejercicio 27.c
@@ -15,4 +15,18 @@ while (resultnum1!=0)
     digitosnum1++;
     resultnum1/=10;
 }
+while (resultnum2!=0)
+{
+    digitosnum2++;
+    resultnum2/=10;
+}
+if (digitosnum1>digitosnum2)
+{
+    printf ("El numero %d tiene mas digitos que %d\n",num1,num2);
+}
+if (digitosnum2>digitosnum1)
+{
+    printf ("El numero %d tiene mas digitos que %d\n",num2,num1);
+}
+return 0;
 }
