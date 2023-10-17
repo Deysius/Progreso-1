ea8930e0feb902327b8981f9ecd497dda6452aa2
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-16 23:27:47 -0500

    Agrego dos if, para comparar cuando un numero ingresado es mayor al otro

--- Semana3/Ejercicio 18.c
+++ Semana3/Ejercicio 18.c
@@ -2,5 +2,17 @@
 int main(void){
 int numero1;
 int numero2;
+printf ("Ingrese su primer numero:\n");
+scanf ("%d",&numero1);
+printf ("Ingrese su segundo numero:\n");
+scanf ("%d",&numero2);
+if (numero1>numero2)
+{
+    
+}
+if (numero2>numero1)
+{
+    
+}
 return 0;
 }
