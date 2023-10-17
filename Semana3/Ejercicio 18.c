66518c42623710f75fd5068e75881509483fa7d1
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-16 23:31:56 -0500

    Agrego dos bucles for, uno en cada if para poder encontrar todo los multiplos de 5 entre ambos numeros

--- Semana3/Ejercicio 18.c
+++ Semana3/Ejercicio 18.c
@@ -8,10 +8,18 @@ printf ("Ingrese su segundo numero:\n");
 scanf ("%d",&numero2);
 if (numero1>numero2)
 {
-    
+    for (int i=numero2; i>=numero1; i++)
+    {
+        /* code */
+    }
+      
 }
 if (numero2>numero1)
 {
+    for (int i=numero1; i>=numero2; i++)
+    {
+        /* code */
+    }
     
 }
 return 0;
