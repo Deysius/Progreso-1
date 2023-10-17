75d769c9a2b9911229baae876f4c74fb6cf4ac73
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-17 11:38:01 -0500

    Mediante un bucle for, realizo la operacion para encontrar el numero factorial

--- Semana3/Ejercicio 45.c
+++ Semana3/Ejercicio 45.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(void){
 int num;
-int fact;
+int fact=1;
 printf ("Ingrese un numero para encontrar su factorial\n");
 scanf ("%d",&num);
-
+for (int i=1; i<=num; i++)
+{
+    fact*=i;
+}
 return 0;
 }
